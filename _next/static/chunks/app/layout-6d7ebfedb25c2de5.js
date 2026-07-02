d6eeb5689f55f149f3fe5116c84023b00844cc77
Wemade <wemadesmm@hotmail.com>
2026-07-02 18:56:07 +0300

    Revert "Deploy static site to GitHub Pages"
    This reverts commit ae2cb6c8f35a5e17908592118fc3732a5c25c667.

--- _next/static/chunks/app/layout-6d7ebfedb25c2de5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{770:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9408,23))},9408:()=>{}},e=>{e.O(0,[421,764,86,358],()=>e(e.s=770)),_N_E=e.O()}]);
